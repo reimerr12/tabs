69ff796abdab35b88b900c5b0e230f0136ac6149
reimerr12 <sameermahirmomo@gmail.com>
2024-07-11 01:01:52 +0600

    add

---
+++ app.js
@@ -0,0 +1,23 @@
+//get items
+
+const btn = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener('click',function(e){
+    const id = e.target.dataset.id;
+
+    if(id){
+        //remove active from all buttons
+        btn.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // other articles hide
+        articles.forEach(function(article){
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    };
+})
